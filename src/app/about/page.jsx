fd61661d34427066b6511b544c00e6d57b70e93f
lFobas <mishashuptar@gmail.com>
2024-05-01 15:53:22 +0300

    add vercel SpeedInsights

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,11 @@
 import HeroAbout from '@/components/HeroAbout'
 import React from 'react'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const page = () => {
   return (
     <div className='p-3'>
+      <SpeedInsights/>
       <HeroAbout />
     </div>
   )
